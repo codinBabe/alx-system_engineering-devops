b1c97e6bbbf8e9553e85f935878c0c2ea442d350
codinBabe <teboola2@gmail.com>
2024-03-22 23:35:59 +0100

    1-install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 # Using puppet to install a package
-package{'python3-pip':
-ensure => 'installed',
-}
-
 package{'flask':
-ensure   => 'installed',
-provider => 'pip',
-require  => Package['python3-pip'],
+ensure   => '2.1.0',
+provider => 'pip3',
 }

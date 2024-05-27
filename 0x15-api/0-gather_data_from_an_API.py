b348f0839e7258614e63b927347965cf137ce30e
codinBabe <teboola2@gmail.com>
2024-05-27 10:31:11 -0700

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,22 @@
 import requests
 import sys
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+    #the url endpoint
     url = "https://jsonplaceholder.typicode.com/"
+
+    # get request for user info
     user = requests.get(url + "user/{}".format(sys.argv[1])).json()
+
+    # get request for todo list
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    # check for completed todo list
     completed = [tsk.get("title") for tsk in todos if tsk.get("completed") is True]
+
+    #print employee name, completed tasks & total no of tasks
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
+
+    # print the titles of completed task with indentation
     [print("\t {}".format(i)) for i in completed]

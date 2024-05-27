a95cc1566f942ce2b2e517095a5374150ac64a89
codinBabe <teboola2@gmail.com>
2024-05-27 11:31:10 -0700

    2-csv

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,22 +4,23 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    # the url endpoint
+    # Define the URL for the REST API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # get request for user info
+    # send a GET request to retrieve user info
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
 
-    # get request for todo list
+    # send a GET request to retrive the TODO list
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    # check for completed todo list
+    # filter completed TODO list and store titles in a list
     completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-    # print employee name, completed tasks & total no of tasks
+    # print employee's name, completed tasks & total no of tasks
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
 
-    # print the titles of completed task with indentation
-    [print("\t {}".format(i)) for i in completed]
+    # print the titles of completed tasks with indentation
+    [print("\t {}".format(c)) for c in completed]

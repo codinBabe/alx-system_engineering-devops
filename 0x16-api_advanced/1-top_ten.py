f8eef73a29c25ee10274fb051ed4317d72800600
codinBabe <teboola2@gmail.com>
2024-06-21 21:44:37 -0700

    completed

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+get the top ten hot topics of a subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    get the top ten hot topics of a subreddit
+    """
+    results = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10",
+        headers={"User-Agent": "my apibot 0.1"})
+    if results.status_code == 200:
+        response_data = results.json()
+        titles = response_data['data']['children']
+        for title in titles:
+            print(title['data']['title'])
+    else:
+        print(None)
